485a4fc90a07d47574afa295e310fba2790ccff6
boriskisiov <boriskisiov@gmail.com>
2023-10-03 17:07:46 +0300

    Add project files.

---
+++ Vowel or Consonant/Program.cs
@@ -0,0 +1,19 @@
+﻿string let = Console.ReadLine();
+
+switch (let)
+{
+        case "A":
+        case "a": 
+        case "E":
+        case "e":
+        case "I":
+        case "i":
+        case "O":
+        case "o":
+        case "U":
+        case "u":
+        Console.WriteLine("Vowel");
+        break;
+        default:Console.WriteLine("Consonan");
+        break;
+}
